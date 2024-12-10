516c4084a61a8fc56f87ac41633fdc371c41aec7
unknown <bondmandev@gmail.com>
2024-12-10 10:11:32 +0300

    modif v45

--- app/Create/[id]/page.tsx
+++ app/Create/[id]/page.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 
-// const getData = async (id: string) => {
-//   const res = await fetch("http://localhost:3000/blog/" + id);
-//   const data = await res.json();
-//   return data;
-// };
-export default async function page({ params }: { params: { id: string } }) {
-  // const article = await getData(params.id);
-  // console.log(article);
+type Props = {
+  params: {
+    id: number;
+  };
+};
+const getData = async (id: number) => {
+  const res = await fetch("http://localhost:3000/blog/" + id);
+  const data = await res.json();
+  return data;
+};
+export default async function page({ params }: Props) {
+  const article = await getData(params.id);
+  console.log(article);
 
   return (
     <div className="bg-slate-200 m-8">
       <div className="p-8 text-center">
-        <div>{params.id}</div>
-        <div className="text-right">{params.id}</div>
+        <div>{article.title}</div>
+        <div className="text-right">{article.auteur}</div>
       </div>
     </div>
   );
